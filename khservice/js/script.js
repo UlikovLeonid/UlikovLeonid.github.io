4746f60dd3a5a2ad0a88e3f48a7cd70026368f8d
Leo <l.ulikov@mail.ru>
2020-03-12 17:28:52 +0300

    без jquery слайдера

--- khservice/js/script.js
+++ khservice/js/script.js
@@ -1,9 +1,17 @@
-$('.sl').slick({
-	autoplay: true,
-	autoplaySpeed: 3000,
-	dots: true,
-	arrows: false,
-	speed: 2000,
-	pauseOnDotsHover: true
-});
-console.log(1);
+
+var left = 0;
+var timer;
+
+function autoSlider() {
+	timer = setTimeout(function () {
+		let line = document.getElementById('line');
+		left = left - 1000;
+		if (left < -3000) {
+			left = 0;
+			clearTimeout(timer);
+		}
+		line.style.left = left + 'px';
+		autoSlider();
+	}, 3000);
+}
+autoSlider();
